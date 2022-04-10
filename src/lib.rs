b0b71a2ce6c055b65afd35c7abc8e2c4862b2c69
yinguobing <yinguobing@gmail.com>
2022-04-10 17:35:19 +0800

    scan the directory recursively

--- src/lib.rs
+++ src/lib.rs
@@ -32,21 +32,32 @@ impl Config {
     }
 }
 
+// Scan the target path and count all the files.
+fn scan(path: &Path, counter: &mut HashMap<String, usize>) -> Result<(), Box<dyn Error>> {
+    let entries = fs::read_dir(path)?;
+    for entry in entries {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            scan(path.as_path(), counter);
+        } else {
+            if let Some(extension) = path.extension() {
+                let extension = extension.to_str().unwrap().to_string();
+                let count = counter.entry(extension).or_insert(0);
+                *count += 1;
+            }
+        }
+    }
+    Ok(())
+}
+
 // Count all the files in this function.
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     // Use a hashmap to record different files count.
     let mut counter: HashMap<String, usize> = HashMap::new();
+    let target_path = Path::new(&config.target_path);
+    scan(&target_path, &mut counter);
     println!("Counting files in {}", config.target_path);
-    let entries = fs::read_dir(config.target_path.clone())?;
-    for entry in entries {
-        let entry = entry?;
-        let path = entry.path();
-        if let Some(extension) = path.extension() {
-            let extension = extension.to_str().unwrap().to_string();
-            let count = counter.entry(extension).or_insert(0);
-            *count += 1;
-        }
-    }
     println!("Total files: {:?}", counter);
     Ok(())
 }

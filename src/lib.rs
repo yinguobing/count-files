85c72ba27f3800a84c18b49e337a2e6eb8cb7a84
yinguobing <yinguobing@gmail.com>
2022-04-10 16:21:19 +0800

    move core code into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+// Config struct converts the user inputs into arguments.
+pub struct Config {
+    pub target_path: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        // Target dir provided?
+        if args.len() < 2 {
+            return Err(
+                "target directory not assigned.\nTry running like this:\n   count_files /path/to/dir",
+            );
+        }
+        // Target dir exist?
+        let target_path = args[1].to_string();
+        if !Path::new(&target_path).exists() {
+            return Err("target directory not found.");
+        }
+        // All is well.
+        Ok(Self {
+            target_path: target_path,
+        })
+    }
+}
+
+// Count all the files in this function.
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    println!("Counting files in {}", config.target_path);
+    let entries = fs::read_dir(config.target_path.clone())?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
-use std::path::Path;
 use std::process;
-use std::fs;
-use std::error::Error;
+
+use count_files::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,34 +15,4 @@ fn main() {
         println!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-// Config struct converts the user inputs into arguments.
-struct Config {
-    target_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        // Target dir provided?
-        if args.len() < 2 {
-            return Err(
-                "target directory not assigned.\nTry running like this:\n   count_files /path/to/dir",
-            );
-        }
-        // Target dir exist?
-        let target_path = args[1].to_string();
-        if !Path::new(&target_path).exists() {
-            return Err("target directory not found.");
-        }
-        // All is well.
-        Ok(Self {target_path: target_path})
-    }
-}
-
-// Count all the files in this function.
-fn run(config: &Config) -> Result<(), Box<dyn Error>> {
-    println!("Counting files in {}", config.target_path);
-    let entries = fs::read_dir(config.target_path.clone())?;
-    Ok(())
 }
